04c7a402a724acd0c151cd4aafdfe94453f68a6c
FernandoHurtado <mrfe333@gmail.com>
2019-04-30 23:47:28 -0500

    ultra rough brainstorm

---
+++ PVM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 20:19:13 2019
+
+@author: f
+"""
+
+class ExpectedValue (BCS, BCS-Probability, WCS, WCS-Probability):
+    return ((BCS * BCS-Probability)+(WCS * WCS-Probability))
+    
+
+class Utilitarianism (CommonGood, IndividualCompromise):
+        
+    
+class Egoism (CommonGood, IndividualCompromise)
+    
+
+def main():
+    
+    
+main():
