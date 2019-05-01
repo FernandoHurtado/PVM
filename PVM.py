3b077d048a28147994b79b9292ed915c62edf145
FernandoHurtado <mrfe333@gmail.com>
2019-05-01 17:25:13 -0500

    better code

--- PVM.py
+++ PVM.py
@@ -5,6 +5,9 @@ Created on Tue Apr 30 20:19:13 2019
 
 @author: f
 """
+from collections import namedtuple
+Values = namedtuple('Budget','Probability','Approval','BCS_CG','BCS_IG', 'WCS_CG', 'WCS_IG')
+MedicareForAll = Values(Budget = 20,Probability = 90,Approval = 50,BCS_CG=100,BCS_IG = -20,WCS_CG=50,WCS_IG=-90)
 
 
 
@@ -13,45 +16,68 @@ class DesicionTheory ():
         return ((BCS * Probability)+(WCS * (1 - Probability)))        
     
 class Ethics (): 
-    def Utilitarianism (CommonGood, IndividualGood, Approval, BCS = True):
-        value = 0
-        IndividualGood /= 10
-        Approval /= 10
-        value = CommonGood + IndividualGood + Approval 
-        return value
+    def Select (Values, name):
+        if (name == "Utilitarianism"):
+            return Ethics.Utilitarianism(Values)
+            
+    def Utilitarianism (Values):
+        # BCS
+        value_BC = 0
+        value_BC = Values.BC_IG / 10
+        value_BC += Values.Approval / 10
+        value_BC += Values.BC_CG
+        # WCS
+        value_WC = 0
+        value_WC = Values.WC_IG / 10
+        value_WC += Values.Approval / 10
+        value_WC += Values.WC_CG
         
-    def Egoism (CommonGood, IndividualGood, Approval, BCS = True):
-        value = 0    
-        CommonGood /= 10
-        Approval /= 10
-        value = IndividualGood + CommonGood + Approval       
-        return value
+        return value_BC, value_WC
         
-    def IdealUtilitarianism (CommonGood, IndividualGood, Approval, BCS = True):
-        value = Ethics.Utilitarianism(CommonGood, IndividualGood, Approval)
-        if BCS != True:    
-            return value / 10
-        return value
-
+    def IdealUtilitarianism (Values):
+        # BCS
+        value_BC = 0
+        value_BC = Values.BC_IG / 10
+        value_BC += Values.Approval / 10
+        value_BC += Values.BC_CG
+        # WCS
+        value_WC = 0
+        value_WC = Values.WC_IG / 10
+        value_WC += Values.Approval / 10
+        value_WC += Values.WC_CG
+        value_WC /= 10
+        
+        return value_BC, value_WC
+    
+    def Egoism (Values):
+        # BCS
+        value_BC = 0
+        value_BC = Values.BC_CG / 10
+        value_BC += Values.Approval / 10
+        value_BC += Values.BC_IG
+        # WCS
+        value_WC = 0
+        value_WC = Values.WC_CG / 10
+        value_WC += Values.Approval / 10
+        value_WC += Values.WC_IG
+        
+        return value_BC, value_WC
+    
+    
+    def MoralRelativism (CommonGood, IndividualGood, Approval, BCS = True):
+        return 0
     
     
 def main(): # for testing purpouses 
-    # Medicare for all
-    Budget = 20
-    Probability = 90
-    Approval = 50
-    BCS_CG = 100
-    BCS_IG = -20
-    WCS_CG = 50
-    WCS_IG = -90
+    Evaluate(MedicareForAll, 'Utilitarianism')
     
+def Evaluate(Values, name):
     
-    BCS = Ethics.IdealUtilitarianism(BCS_CG, BCS_IG, Approval, True)
-    WCS = Ethics.IdealUtilitarianism(WCS_CG, WCS_IG, Approval, False)
+    BCS, WCS = Ethics.Select(Values, name)
+
+    EV = DesicionTheory.ExpectedValue(BCS, WCS, Values.Probability / 100)
     
-    EV = DesicionTheory.ExpectedValue(BCS, WCS, Probability / 100)
-    
-    if EV > Budget:
+    if EV > Values.Budget:
         print("law passes")
     else:
         print("law doesn't pass")
